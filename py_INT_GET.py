87ed5055f0372e6dc83bad7a7c6e165cb8934fd0
Jeffrey <jeffrey.darling15@gmail.com>
2022-12-14 15:42:20 -0800

    Create py_INT_GET.py

---
+++ py_INT_GET.py
@@ -0,0 +1,7 @@
+import requests
+import sys
+import urllib3
+proxies = {'http' : 'http://127.0.0.1:8080', 'https': 'https://127.0.0.8080'}
+r = requests.get("http://info.cern.ch", verify=False, proxies=proxies )
+print(r) # Return code
+print(r.text) # Return page data
